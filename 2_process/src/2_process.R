c1240f9b307e828c389c36cab8c472570be2210d
Koenig <lkoenigsnyder@usgs.gov>
2021-10-28 13:58:27 -0400

    refactor pipeline to filter all salinity data before subsetting SpC data

--- 2_process/src/2_process.R
+++ 2_process/src/2_process.R
@@ -1,36 +1,31 @@
-summarize_wqp_salinity_data <- function(data){
+filter_wqp_salinity_data <- function(data,major_ion_names,select_wqp_vars,omit_wqp_events,exclude_tidal=TRUE){
   
-  records_summary <- data %>% 
-    filter(final=="retain") %>%
-    filter(param_group=="Salinity"|param=="Chloride"|param=="Sodium") %>% 
-    group_by(param_group,param) %>% 
-    summarize(n_records = n(),n_sites = length(unique(MonitoringLocationIdentifier)),.groups="keep") 
-  
-  return(records_summary)
-  
-}
-
-subset_wqp_major_ions_data <- function(data,param,select_columns){
-  
-  data_subset <- filter(data,param==param,final=="retain") %>%
-    select(all_of(select_columns))
+  data_subset <- filter(data,(param_group=="Salinity"|param %in% major_ion_names),
+                        # Filter out any sediment samples, or samples representing hydrologic events that are not of interest
+                        ActivityMediaName!="Sediment",!HydrologicEvent %in% omit_wqp_events,
+                        # Keep QA/QC'ed data deemed reliable
+                        final=="retain") %>%
+    # Filter out any tidal samples if exclude_tidal assignment = TRUE:
+    {if(exclude_tidal==TRUE) filter(.,!grepl("tidal", MonitoringLocationTypeName,ignore.case = TRUE)) else .} %>% 
+    select(all_of(select_wqp_vars))
   
   return(data_subset)
   
 }
 
 
-subset_wqp_spC_data <- function(data,fileout,select_columns){
+subset_wqp_spC_data <- function(filtered_data,fileout){
   
   # Filter out specific conductance param values "min" and "max"
   SpC_params <- c("Specific conductance, field",
                   "Specific conductance",
                   "Specific conductance, field, mean",
                   "Specific conductance, lab")
-  
-  spC_data_subset <- filter(data,param_group=="Salinity",param %in% SpC_params,final=="retain") %>%
-    select(all_of(select_columns))
-  
+
+  spC_data_subset <- filtered_data %>%
+    # Omit samples originally entered as "conductivity" since we can't be sure these reflect temperature-corrected conductance
+    filter(param %in% SpC_params,CharacteristicName!="Conductivity") 
+
   write_csv(spC_data_subset, file = fileout)
   
   return(fileout)
--- _targets.R
+++ _targets.R
@@ -9,9 +9,15 @@ dir.create("2_process/out/", showWarnings = FALSE)
 dir.create("3_visualize/out/", showWarnings = FALSE)
 
 # Define columns of interest for harmonized WQP data
-select_columns <- c("MonitoringLocationIdentifier","LongitudeMeasure","LatitudeMeasure",
-                    "CharacteristicName","param","USGSPCode","ActivityStartDate",
-                    "ActivityEndDate","resultVal2","resultUnits2","PrecisionValue")
+select_wqp_vars <- c("MonitoringLocationIdentifier","MonitoringLocationName","LongitudeMeasure","LatitudeMeasure",
+                     "MonitoringLocationTypeName","OrganizationIdentifier","ActivityStartDate","ActivityStartTime.Time",
+                     "ActivityEndDate","CharacteristicName","param_group","param","USGSPCode","ActivityMediaName",
+                     "ResultSampleFractionText","HydrologicCondition","HydrologicEvent","resultVal2","resultUnits2",
+                     "ResultDetectionConditionText","ResultTemperatureBasisText","PrecisionValue","ResultStatusIdentifier",
+                     "final")
+
+# Define hydrologic event types in harmonized WQP data to exclude
+omit_wqp_events <- c("Spill","Volcanic action")
 
 list(
   # Load harmonized WQP data product
@@ -19,20 +25,15 @@ list(
     p1_wqp_data,
     readRDS(file = "1_fetch/in/DRB.WQdata.rds")
   ),
-  # Summarize data related to salinity in harmonized WQP data
+  # Filter salinity data within harmonized WQP data
   tar_target(
-    p2_wqp_salinity_records_table,
-    summarize_wqp_salinity_data(p1_wqp_data)
-  ),
-  # Subset discrete major ions data
-  tar_target(
-    p2_wqp_Cl,
-    subset_wqp_major_ions_data(p1_wqp_data,param="Chloride",select_columns)
+    p2_filtered_wqp_data,
+    filter_wqp_salinity_data(p1_wqp_data,major_ion_names = c("Chloride","Sodium"),select_wqp_vars,omit_wqp_events)
   ),
   # Subset and save discrete specific conductance data
   tar_target(
     p2_wqp_spC_csv,
-    subset_wqp_spC_data(p1_wqp_data,fileout="2_process/out/DRB_WQdata_spC_data.csv",select_columns)
+    subset_wqp_spC_data(p2_filtered_wqp_data,fileout="2_process/out/DRB_WQdata_spC_data.csv")
   ),
   # Render data summary report
   tarchetypes::tar_render(p3_wqp_spC_report, "3_visualize/src/report-wqp-salinity-data.Rmd",output_dir = "3_visualize/out")
