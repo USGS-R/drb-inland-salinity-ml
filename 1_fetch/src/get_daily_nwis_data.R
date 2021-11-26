320521ff08103afb4913907200c649046e8ed3de
Koenig <lkoenigsnyder@usgs.gov>
2021-11-26 14:51:35 -0500

    Improve documentation and update daily data download to use dynamic branching

---
+++ 1_fetch/src/get_daily_nwis_data.R
@@ -0,0 +1,60 @@
+get_daily_nwis_data <- function(site_info,parameter,stat_cd_select) {
+  #' 
+  #' @description Function to download NWIS daily data
+  #'
+  #' @param site_info data frame containing site info for NWIS daily site, including the variable "site_no"
+  #' @param parameter a character vector containing the USGS parameter codes of interest
+  #' @param stat_cd_select a character vector containing the USGS stat codes to retain
+  #'
+  #' @value A data frame containing daily values and data quality codes for each stat code (e.g. min/max/mean)
+  #' @examples 
+  #' get_daily_nwis_data(site_info = daily_sites,parameter="00300",stat_cd_select="00003")
+  
+  message(sprintf('Retrieving daily data for %s', site_info$site_no))
+
+  # Download daily data
+  site_data <- dataRetrieval::readNWISdv(
+    siteNumbers = site_info$site_no,parameterCd=parameter,statCd=stat_cd_select,startDate = "",endDate = "") %>%
+    dataRetrieval::renameNWISColumns(p00300="Value",p00095="Value")
+  
+  # Munge column names for some sites with different or unusually-named value columns
+  if(length(grep("Value_cd",names(site_data)))>1){
+    site_data <- switch(site_info$site_no[1],
+                        "01463500" = site_data %>%
+                          mutate(Value_merged = coalesce(`Value`,`..2.._Value`),
+                                 Value_cd_merged = coalesce(`Value_cd`,`..2.._Value_cd`),
+                                 Value_Max_merged = coalesce(`Value_Max`,`..2.._Value_Max`),
+                                 Value_Max_cd_merged = coalesce(`Value_Max_cd`,`..2.._Value_Max_cd`)) %>%
+                          select(agency_cd,site_no,Date,Value_merged,Value_cd_merged,Value_Max_merged,Value_Max_cd_merged) %>%
+                          rename("Value"="Value_merged","Value_cd" = "Value_cd_merged","Value_Max"="Value_Max_merged","Value_Max_cd"="Value_Max_cd_merged"),
+                        "01467200" = site_data %>%
+                          mutate(Value_merged = coalesce(`Value`,`ISM.Test.Bed...ISM.barge._Value`),
+                                 Value_cd_merged = coalesce(`Value_cd`,`ISM.Test.Bed...ISM.barge._Value_cd`)) %>%
+                          select(agency_cd,site_no,Date,Value_merged,Value_cd_merged,Value_Max,Value_Max_cd) %>%
+                          rename("Value"="Value_merged","Value_cd"="Value_cd_merged"),
+                        "01412350" = site_data %>%
+                          mutate(Value = `.Discontinued._Value`,
+                                 Value_cd = `.Discontinued._Value_cd`,
+                                 Value_Max = `.Discontinued._Value_Max`,
+                                 Value_Max_cd = `.Discontinued._Value_Max_cd`) %>%
+                          select(agency_cd,site_no,Date,Value,Value_cd,Value_Max,Value_Max_cd))
+  }
+  
+  # If no max value reported, create empty column:
+  if(!('Value_Max' %in% names(site_data))){
+    site_data <- site_data %>%
+      add_column(Value_Max = NA,
+                 Value_Max_cd = NA)
+  }
+  
+  # Filter daily data
+  site_data_out <- site_data %>%
+           # omit rows with no data
+    filter(!(is.na(Value) & is.na(Value_Max)),
+           # omit rows where daily mean > daily max
+           (is.na(Value)|is.na(Value_Max)|(!Value > Value_Max))) %>%
+    mutate(Parameter=c("00095"="SpecCond","00300"="DO")[parameter]) %>%
+    select(agency_cd,site_no,Date,Parameter,Value,Value_cd,Value_Max,Value_Max_cd)
+  
+  return(site_data_out)
+}
--- _targets.R
+++ _targets.R
@@ -11,7 +11,7 @@ dir.create("2_process/out/", showWarnings = FALSE)
 dir.create("3_visualize/out/", showWarnings = FALSE)
 
 # Define columns of interest for harmonized WQP data
-select_wqp_vars <- c("MonitoringLocationIdentifier","MonitoringLocationName","LongitudeMeasure","LatitudeMeasure",
+wqp_vars_select <- c("MonitoringLocationIdentifier","MonitoringLocationName","LongitudeMeasure","LatitudeMeasure",
                      "MonitoringLocationTypeName","OrganizationIdentifier","ActivityStartDate","ActivityStartTime.Time",
                      "ActivityEndDate","CharacteristicName","param_group","param","USGSPCode","ActivityMediaName",
                      "ResultSampleFractionText","HydrologicCondition","HydrologicEvent","resultVal2","resultUnits2",
@@ -24,19 +24,25 @@ major_ion_names = c("Chloride","Sodium")
 # Define hydrologic event types in harmonized WQP data to exclude
 omit_wqp_events <- c("Spill","Volcanic action")
 
-# Define USGS specific conductance parameter codes
-SpC_pcodes <- c("00095","90095","00094","90096") 
+# Define all USGS specific conductance parameter codes
+pcodes_select <- c("00095","90095","00094","90096") 
 
-# Define minor HUCs (hydrologic unit codes) that make up the DRB (HUC04 = 0204)to use in calls to dataRetrieval functions
+# Define USGS specific conductance parameter code to use for continuous data
+parameter <- "00095"
+
+# Define minor HUCs (hydrologic unit codes) that make up the DRB to use in calls to dataRetrieval functions
+# Lower Delaware: 0204 subregion (for now, exclude New Jersey Coastal (https://water.usgs.gov/GIS/huc_name.html) and Delaware Bay ("02040204")
 drb_huc8s <- c("02040101","02040102","02040103","02040104","02040105","02040106",
-               "02040201","02040202","02040203","02040204","02040205","02040206","02040207",
-               "02040301","02040302")
+               "02040201","02040202","02040203","02040205","02040206","02040207")
 
 # Define USGS site types for which to download specific conductance data (include "Stream","Stream:Canal", and "Spring" sites)
 site_tp_select <- c("ST","ST-CA","SP") 
 
-# Change dummy date to force re-build of relevant NWIS sites
-dummy_date <- "2021-11-12"
+# Define USGS stat codes for continuous sites that only report daily statistics (https://help.waterdata.usgs.gov/stat_code) 
+stat_cd_select <- c("00001","00003")
+
+# Change dummy date to force re-build of NWIS SC sites and data download
+dummy_date <- "2021-11-23"
 
 # Return the complete list of targets
 c(p1_targets_list, p2_targets_list, p3_targets_list)
