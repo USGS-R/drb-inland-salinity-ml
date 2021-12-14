79a44061ec1573956eb610feae485cb7f89ef520
Koenig <lkoenigsnyder@usgs.gov>
2021-12-14 16:15:20 -0500

    add discrete obs-day count to sitelist_summary target

--- 3_visualize/src/summarize_site_list.R
+++ 3_visualize/src/summarize_site_list.R
@@ -17,22 +17,26 @@ summarize_site_list <- function(site_list_path,nwis_daily_data,nwis_inst_data,fi
   # Summarize data availability
   site_summary <- site_list %>%
     summarize(
-           # tally total number of WQP sites that contribute discrete data
-           n_all_sites_WQP = as.numeric(tally(filter(.,grepl("WQP",.$data_src_combined,ignore.case=TRUE)))),
-           # tally total number of NWIS-daily sites that contribute continuous data
-           n_all_sites_nwis_daily = length(unique(nwis_daily_data$site_no)),
-           # tally total number of NWIS-inst sites that contribute continuous data
-           n_all_sites_nwis_inst = length(unique(nwis_inst_data$site_no)),
-           # tally number of unique NWIS sites that contribute continuous data (daily or inst)
-           n_unique_sites_NWIS = as.numeric(tally(filter(.,grepl("NWIS",.$data_src_combined,ignore.case=TRUE)))),
-           # tally number of site id's that have observations from both NWIS and WQP
-           n_sites_intersect_WQPNWIS = as.numeric(tally(filter(.,.$data_src_combined %in% c("NWIS_daily/Harmonized_WQP_data","NWIS_instantaneous/Harmonized_WQP_data")))),
-           # tally number of unique site id's across data sources
-           n_unique_site_ids = length(unique(site_id)),
-           # tally number of unique lat/lon locations across data sources
-           n_unique_latlon = as.numeric(tally(distinct(.,lat,lon))),
-           # tally total number of observation-days across data sources
-           n_obsdays = sum(count_days_total))
+      # tally number of unique site id's across data sources
+      n_unique_siteids = length(unique(site_id)),
+      # tally number of unique lat/lon locations across data sources
+      n_unique_latlon = as.numeric(tally(distinct(.,lat,lon))),
+      # tally total number of WQP sites that contribute discrete data
+      n_all_WQP_sites = as.numeric(tally(filter(.,grepl("WQP",.$data_src_combined,ignore.case=TRUE)))),
+      # tally total number of NWIS-daily sites that contribute continuous data
+      n_all_nwis_daily_sites = length(unique(nwis_daily_data$site_no)),
+      # tally total number of NWIS-inst sites that contribute continuous data
+      n_all_nwis_inst_sites = length(unique(nwis_inst_data$site_no)),
+      # tally number of unique NWIS sites that contribute continuous data (daily or inst)
+      n_unique_nwis_sites = as.numeric(tally(filter(.,grepl("NWIS",.$data_src_combined,ignore.case=TRUE)))),
+      # tally number of site id's that have observations from both NWIS and WQP
+      n_sites_intersect_WQPNWIS = as.numeric(tally(filter(.,.$data_src_combined %in% c("NWIS_daily/Harmonized_WQP_data","NWIS_instantaneous/Harmonized_WQP_data")))),
+      # tally total number of observation-days across data sources
+      n_obsdays_total = sum(count_days_total,na.rm=TRUE),
+      # tally number of observation-days from NWIS
+      n_obsdays_nwis = sum(count_days_nwis,na.rm=TRUE),
+      # tally number of discrete sample observation-days
+      n_obsdays_discrete = sum(count_days_discrete,na.rm=TRUE))
   
   # Save data availability log file
   write_csv(site_summary, file = fileout)
