b86391b0f59916be5f88de0c42c1b0b526f76d14
Margaux Sleckman <msleckman@usgs.gov>
2021-12-29 14:45:50 -0800

    simplifaction

--- 2_process/src/combine_NLCD_PRMS.R
+++ 2_process/src/combine_NLCD_PRMS.R
@@ -1,37 +1,40 @@
-## Gather land cover attributes for PRMS segments
-library(readr)
-library(sf)
-library(targets)
-library(tidyverse)
-library(data.table)
-
 ###-----
-LC_w_area <- function(area_att, NLCD_LC_df, drb_comids_df){
+## Subset Land to AOI and join with area and PRMS ids
+AOI_LC_w_area <- function(NLCD_LC_df, area_att, aoi_comids_df){
   
-# Filter land cover attributes for DRB comids that intersect PRMS segments and join with area columns
-  lc_aoi <- NLCD_LC_df %>%
+  #' @description following the comid id as the main key, this function subset NLCD LC data to the area of interest, join with area info and PRMS
+  #' Output is a df with PRMS ids, COMIDs, AREA, and NLCD LC percentage per COMID (unique key)
+  #' @param area_att df with comid and associated area (AREASQKM)
+  #' @param NLCD_LC_df selected LC dataframe organized by COMID 
+  #' @param aoi_comids_df df linking comid and prms for the area of interest, already filter to area of interest 
+  #' @example AOI_LC_w_area(NLCD_LC_df = p1_NLCD_df$NLCD_LandCover_2011, area_att = p2_nhd_area_att, drb_comids_df = p2_drb_comids)
+  
+# Filter land cover attributes to aoi comids (drb) and join with area columns
+  lc_area <- NLCD_LC_df %>%
     filter(COMID %in% drb_comids_df$comid) %>%
     left_join(., area_att, by="COMID") %>%
     mutate(COMID = as.character(COMID))
-  
+
+# Create df with PRMS, COMID, and NLCD LC att for the aoi (drb)
+  lc_aoi <- aoi_comids_df %>%
+    left_join(., lc_area, by = c('comid' = "COMID"))
+
   return(lc_aoi)
-}
+
+  }
 
 ###----
+# Estimate proportion of LC coverage at PRMS scale
 proportion_lc_by_prms <- function(NLCD_LC_df_w_area,
-                                  drb_comids_df,
                           catchment_att = "CAT"){
 
-#' @description
-#' @param NLCD_LC_df_w_area
-#' @param catchment_att Must be either 'CAT', "ACC", 'TOT'
-#' @example 
-
+#' @description This function takes the 
+#' @param NLCD_LC_df_w_area df output of AOI_LC_w_area that has LC data per COMID & PRMS id and area in (km2)
+#' @param catchment_att Specifies the catchment attribute to subset to. Must be either 'CAT', "ACC", 'TOT'. Default = "CAT"
+#' @example proportion_lc_by_prms(NLCD_LC_df_w_area = p2_LC_w_catchment_area, catchment_att = "CAT")
 
 # Try out land cover aggregation for just one NLCD class 
-area_df <- drb_comids_df %>%
-  # Joining PRMS with NLCD LC 
-  left_join(.,NLCD_LC_df_w_area,by=c("comid"="COMID")) %>%
+area_df <- NLCD_LC_df_w_area %>%
   # multiply area by percentage for all the cols that start with 'CAT" or "ACC" or "TOT"
   mutate(across(starts_with(catchment_att),~(.x/100)*AREASQKM,.names="AREA_{col}")) %>%
   # aggregate to PRMS scale through group_by()
