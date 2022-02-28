6c83a570e8579c127c243dadf88fc00278d89abc
Koenig <lkoenigsnyder@usgs.gov>
2022-02-28 14:43:24 -0500

    remove NA tally from NHDv2 attr summary table

--- 3_visualize/src/summarize_nhdv2_attr.R
+++ 3_visualize/src/summarize_nhdv2_attr.R
@@ -7,21 +7,14 @@ summarize_nhdv2_attr <- function(attr_data,fileout){
   #' @param fileout a character string that indicates the name of the output file, including path and extension
   #'
   #' @value Returns a csv file containing summary statistics for each NHDv2 attribute variable
-  
-
-  # Define function to summarize the number of NA's in numeric vector x
-  # The argument `na.rm = TRUE` is included so that the `summarize_at` line will run in the code
-  # chunk below that generates `attr_summary`; it does not remove NA values in `num_NA()`
-  num_NA <- function(x,na.rm=TRUE){
-    sum(is.na(x))
-  }
+  #' 
   
   # Calculate summary statistics for each variable's time series
   attr_summary <- attr_data %>%
     select(where(is.numeric)) %>%
     pivot_longer(everything()) %>%
     group_by(name) %>%
-    summarize_at(vars(value), list(Min = min, Mean = mean, Max = max, Sd = sd, num_NA = num_NA),na.rm=TRUE) %>%
+    summarize_at(vars(value), list(Min = min, Mean = mean, Max = max, Sd = sd),na.rm=TRUE) %>%
     mutate_if(is.numeric, round, 3)
   
   # Save data summary
