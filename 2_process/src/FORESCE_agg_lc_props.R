3d00b56c9ac8703c4ee08aecfbe79a2fd2bc8113
Jared D. Smith <jsmith@usgs.gov>
2022-04-05 15:14:30 -0400

    fix proportion unit

--- 2_process/src/FORESCE_agg_lc_props.R
+++ 2_process/src/FORESCE_agg_lc_props.R
@@ -10,7 +10,7 @@ aggregate_proportions_hrus <- function(df, group_by_segment_colname, proportion_
   
   df <- df %>% 
     # Create temp cols of area of lc class per hru - NOTE: simply mutate current cols and no longer have proportion values
-    mutate(across(starts_with(proportion_col_prefix),  ~(.x * {{hru_area_colname}})))%>% 
+    mutate(across(starts_with(proportion_col_prefix),  ~(.x * {{hru_area_colname}}/10^6)))%>% 
     # group by hru segments - droping from 761 row to 416 - to get a single "PRMS" catchment per PRMS segment
     group_by_at(group_by_segment_colname) %>%
     summarise(
