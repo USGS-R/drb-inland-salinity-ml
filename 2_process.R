f7cdbf7740aae7206fe5e75e89be32821fb1698c
Jared D. Smith <jsmith@usgs.gov>
2022-07-18 11:08:51 -0400

    rename seg as PRMS_segid

--- 2_process.R
+++ 2_process.R
@@ -541,6 +541,7 @@ p2_targets_list <- list(
   #Join attributes to SC observations and retain only the days with SC observations
   tar_target(
     p2_all_attr_SC_obs,
-    left_join(p2_SC_observations, p2_all_attr, by = c('subsegid' = 'PRMS_segid', 'Date'))
+    left_join(p2_SC_observations, p2_all_attr, by = c('subsegid' = 'PRMS_segid', 'Date')) %>%
+      rename(PRMS_segid = subsegid)
   )
 )
