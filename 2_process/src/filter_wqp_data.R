2fa8bc4dfb32c19a2ea5eaf53e8f2c8f3adab0ee
Koenig <lkoenigsnyder@usgs.gov>
2022-08-03 19:06:50 -0400

    subset discrete wq data by date range

--- 2_process/src/filter_wqp_data.R
+++ 2_process/src/filter_wqp_data.R
@@ -1,29 +1,47 @@
-filter_wqp_salinity_data <- function(data,major_ion_names,wqp_vars_select,omit_wqp_events,exclude_tidal=TRUE){
+filter_wqp_salinity_data <- function(data,
+                                     major_ion_names,
+                                     wqp_vars_select,
+                                     omit_wqp_events,
+                                     earliest_date,
+                                     latest_date,
+                                     exclude_tidal = TRUE){
   #' 
-  #' @description Function to filter the DRB multisource surface-water-quality dataset for desired parameters, units, and variables  
+  #' @description Function to filter the DRB multisource surface-water-quality dataset for
+  #' desired parameters, units, and variables.
   #'
   #' @param data a data frame containing the downloaded DRB multisource surface-water-quality dataset
-  #' @param major_ion_names a character vector containing the desired parameter values from the data column "param". 
-  #' See Shoda et al. 2019 (https://doi.org/10.5066/P9PX8LZO) for more information.  
-  #' @param wqp_vars_select a character vector indicating which data columns to retain from the DRB multisource dataset
-  #' @param omit_wqp_events a character vector indicating which values to omit from the "HydrologicEvent" column within the DRB multisource dataset
-  #' @param exclude_tidal logical, defaults to TRUE. If TRUE, rows containing "tidal" within the "MonitoringLocationTypeName" column of the
-  #' DRB multisource dataset will be omitted. 
+  #' @param major_ion_names a character vector containing the desired parameter values
+  #' from the data column "param". See Shoda et al. 2019 (https://doi.org/10.5066/P9PX8LZO) 
+  #' for more information.  
+  #' @param wqp_vars_select a character vector indicating which data columns to
+  #' retain from the DRB multisource dataset
+  #' @param omit_wqp_events a character vector indicating which values to omit 
+  #' from the "HydrologicEvent" column within the DRB multisource dataset
+  #' @param earliest_date character string "YY-MM-DD" that indicates the earliest
+  #' sampling date that should be retained. 
+  #' @param latest_date character string "YY-MM-DD" that indicates the most recent
+  #' sampling date that should be retained. 
+  #' @param exclude_tidal logical, defaults to TRUE. If TRUE, rows containing "tidal" 
+  #' within the "MonitoringLocationTypeName" column of the DRB multisource dataset will
+  #' be omitted. 
   #'
-  #' @value A data frame containing discrete water quality samples from the Delaware River Basin for the parameters of interest
-  #' @examples 
-  #' filter_wqp_data(data = DRB_WQdata,params_select=c("Dissolved oxygen"),wqp_vars_select=c("MonitoringLocationIdentifier","MonitoringLocationName"),
-  #' omit_wqp_events=c("Volcanic action"),fileout="./data/out/filtered_wqp_data.csv")
+  #' @value A data frame containing discrete water quality samples from the
+  #' Delaware River Basin for the parameters of interest
+  #' 
   
-  data_subset <- filter(data,(param_group == "Salinity" | param %in% major_ion_names),
-                        # Filter out any sediment samples, samples representing hydrologic events that are not of interest, and samples from LocationType = ditch:
-                        ActivityMediaName != "Sediment",
-                        !(HydrologicEvent %in% omit_wqp_events),
-                        MonitoringLocationTypeName != "Stream: Ditch",
-                        # Keep QA/QC'ed data deemed reliable:
-                        final=="retain") %>%
+  data_subset <- data %>%
+    filter((param_group == "Salinity" | param %in% major_ion_names),
+           # Filter out any sediment samples, samples representing hydrologic events 
+           # that are not of interest, and samples from LocationType = ditch
+           ActivityMediaName != "Sediment",
+           !(HydrologicEvent %in% omit_wqp_events),
+           MonitoringLocationTypeName != "Stream: Ditch",
+           # Keep QA/QC'ed data deemed reliable:
+           final == "retain",
+           ActivityStartDate > earliest_date, 
+           ActivityStartDate < latest_date) %>%
     # Filter out any tidal samples if exclude_tidal = TRUE:
-    {if(exclude_tidal==TRUE){
+    {if(exclude_tidal){
       filter(.,!grepl("tidal", MonitoringLocationTypeName, ignore.case = TRUE))
       } else {.}
     } %>% 
@@ -34,7 +52,7 @@ filter_wqp_salinity_data <- function(data,major_ion_names,wqp_vars_select,omit_w
 }
 
 
-subset_wqp_SC_data <- function(filtered_data,omit_dups = TRUE){
+subset_wqp_SC_data <- function(filtered_data, omit_dups = TRUE){
   #' 
   #' @description Function to subset the filtered WQP salinity dataset for specific conductance  
   #' 
@@ -42,12 +60,14 @@ subset_wqp_SC_data <- function(filtered_data,omit_dups = TRUE){
   #' values of "param" (i.e., 'Specific conductance' vs 'Specific conductance, field') were
   #' not considered duplicates in the data harmonization code. 
   #'
-  #' @param filtered_data a data frame containing the filtered DRB multisource surface-water-quality dataset.
-  #' Filtered_data is the output from filter_wqp_salinity_data().
-  #' @param omit_dups logical indicating whether to omit duplicated observations for a unique site/date-time/
-  #' lat-lon location/collecting organization; defaults to TRUE
+  #' @param filtered_data a data frame containing the filtered discrete water quality
+  #' dataset. `filtered_data` is the output from filter_wqp_salinity_data().
+  #' @param omit_dups logical indicating whether to omit duplicated observations for a 
+  #' unique site/date-time/lat-lon location/collecting organization; defaults to TRUE
   #'
-  #' @value A data frame containing discrete specific conductance samples from the Delaware River Basin 
+  #' @value A data frame containing discrete specific conductance samples from the 
+  #' Delaware River Basin 
+  #' 
   #' @examples 
   #' subset_wqp_SC_data(filtered_data = filtered_wqp_data, omit_dups = TRUE)
 
