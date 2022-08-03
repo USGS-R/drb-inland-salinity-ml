9edfb6024cadc241bdc633d94a1c4fa269e4b83c
Koenig <lkoenigsnyder@usgs.gov>
2022-08-03 19:04:46 -0400

    use updated discrete salinity dataset in 1_fetch/in

--- 1_fetch.R
+++ 1_fetch.R
@@ -36,29 +36,18 @@ p1_targets_list <- list(
     priority = 0.99 # default priority (0.8) is set globally in _targets.R
   ),
   
-  # Load harmonized WQP data product for discrete samples from ScienceBase:
-  # https://www.sciencebase.gov/catalog/item/5d6fa631e4b0c4f70cf92e9e
+  # Load harmonized WQP data product for discrete samples. Note that this data
+  # frame was created from an (internal) DRB data harmonization GitLab repository:
+  # https://code.usgs.gov/wma/iws/wq-data-harmonization/-/commit/b705a2b91877d5e0bb9386250659b46149295b55
+  # The output rds file was downloaded from the inland salinity sharepoint folder
+  # and manually placed in 1_fetch/in.
   tar_target(
-    p1_wqp_data_zip,
-    download_sb_file(sb_id = "5e010424e4b0b207aa033d8c",
-                     file_name = "Water-Quality Data.zip",
-                     out_dir="1_fetch/out"),
+    p1_wqp_data_rds,
+    "1_fetch/in/20220731_DRB_WQdata.rds",
     format = 'file',
     repository = 'local',
     deployment = 'main'
   ),
-  # Unzip zipped file
-  tar_target(
-    p1_wqp_data_rds,
-    {wq_dir = "1_fetch/out/Water_Quality_Data"
-    # `shp_files` is a vector of all files ('dbf', 'prj', 'shp', 'shx')
-    wq_files <- unzip(p1_wqp_data_zip, exdir = wq_dir)
-    # return just the .shp file
-    grep(".rds", wq_files, value = TRUE)},
-    format = "file",
-    deployment = 'main'
-  ),
-  # Read in harmonized WQP data product
   tar_target(
     p1_wqp_data,
     readRDS(file = p1_wqp_data_rds),
