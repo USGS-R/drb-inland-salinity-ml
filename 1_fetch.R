3bee1ee27176ebfa3d3c52d1fd69569e2f609e9c
Jared D. Smith <jsmith@usgs.gov>
2022-09-16 13:24:14 -0500

    aws target edited to include user password flag

--- 1_fetch.R
+++ 1_fetch.R
@@ -30,7 +30,10 @@ p1_targets_list <- list(
   # AWS credentials target
   tar_target(
     p1_aws_credentials_1,
-    {system2("~/saml2aws login --skip-prompt --role='arn:aws:iam::807615458658:role/adfs-wma-developer' --profile='dev' --force --session-duration=28800 --credentials-file='./credentials' --quiet")
+    {pass <- read_table('~/saml2aws-files/pass.txt', col_names = FALSE, show_col_types = FALSE) %>%
+      as.character()
+    system(paste0("~/saml2aws-files/saml2aws login --skip-prompt --role='arn:aws:iam::807615458658:role/adfs-wma-developer' --profile='dev' --force --session-duration=28800 --credentials-file='./credentials' --quiet --password='", pass, "'"))
+    rm(pass)
     #return 0
     0
     },
